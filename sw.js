819af349dcc8e5f8d1ca04c56ab826d13e63454c
valuecabs <rsiddiqui01@qub.ac.uk>
2026-09-01 12:04:41 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'vc-training-v1';
+const ASSETS = [
+  '/valuecabs-training/',
+  '/valuecabs-training/index.html',
+  '/valuecabs-training/quiz.html',
+  '/valuecabs-training/portrait.html'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    fetch(e.request).catch(function(){
+      return caches.match(e.request);
+    })
+  );
+});
